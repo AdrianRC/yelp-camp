eb99101fb8d9c3bc1fcc4a098b4d4fdf2deeb813
AdrianRC <adrian.rosario.codik@gmail.com>
2017-02-19 11:24:39 -0400

    Add mongoose and data persistance to the site.

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var campgrounds = [{
-        name: "Salmon Creek",
-        image: "img/3062207412.png"
-    },
-    {
-        name: "Granite Hill",
-        image: "img/8737935921.png"
-    },
-    {
-        name: "Mountain Goat's Rest",
-        image: "img/9586944536.png"
-    }
-];
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Granite Hill",
+//     image: "img/8737935921.png"
+// }, function (err, campground) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(campground);
+//     }
+// });
 
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {
-        campgrounds: campgrounds
+    Campground.find({}, function (err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {
+                campgrounds: campgrounds
+            });
+        }
     });
 })
 
@@ -39,7 +51,16 @@ app.get("/campgrounds/new", function (req, res) {
 app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    campgrounds.push({name: name, image: image});
+    Campground.create({
+        name: name,
+        image: image
+    }, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(campground);
+        }
+    });
     res.redirect("/campgrounds");
 });
 
