dfd7d6e8d30bf4c574a8a55c3fec40cbe60424dd
AdrianRC <adrian.rosario.codik@gmail.com>
2017-02-22 12:58:50 -0400

    Style the campground show page.

--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ var Comment = require("./modules/comment"),
 var data = [{
         name: "Cloud's Rest",
         image: "https://farm4.staticflickr.com/3211/3062207412_03acc28b80.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra convallis pharetra. In a neque leo. Donec tortor magna, sodales et vestibulum non, efficitur et ligula. Pellentesque non vulputate tortor, ut volutpat urna. Proin placerat, urna eget ultrices elementum, risus nisl euismod leo, sit amet vestibulum est dolor ac diam. Duis nec orci neque. Phasellus dapibus quis orci eu pellentesque. Curabitur vestibulum pulvinar blandit. Quisque vitae ligula lorem."
     },
     {
         name: "Desert Mesa",
         image: "https://farm8.staticflickr.com/7457/9586944536_9c61259490.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra convallis pharetra. In a neque leo. Donec tortor magna, sodales et vestibulum non, efficitur et ligula. Pellentesque non vulputate tortor, ut volutpat urna. Proin placerat, urna eget ultrices elementum, risus nisl euismod leo, sit amet vestibulum est dolor ac diam. Duis nec orci neque. Phasellus dapibus quis orci eu pellentesque. Curabitur vestibulum pulvinar blandit. Quisque vitae ligula lorem."
     },
     {
         name: "Canyon Floor",
         image: "https://farm4.staticflickr.com/3191/3061337059_36c9457ab6.jpg",
-        description: "blah blah blah"
+        description: "Donec at venenatis enim. Nunc tempor mattis tortor, et cursus augue semper ut. Mauris in felis a purus pretium semper. Vestibulum sed venenatis metus, non convallis magna. Quisque interdum suscipit dolor, vitae congue nulla dapibus tincidunt. Etiam nulla neque, dapibus at nisi a, efficitur condimentum nulla. Nunc pretium id ipsum sit amet gravida. Morbi sed quam at nulla rutrum vestibulum at congue turpis. Ut eget faucibus velit. Curabitur eu ligula in ante pretium eleifend. Donec blandit orci orci, sed venenatis eros ultricies vitae. Morbi eget neque eleifend, finibus urna in, porttitor libero."
     }
 ];
 
