71805fbca1094a702bc5831786b55e68398bc532
AdrianRC <adrian.rosario.codik@gmail.com>
2017-02-25 22:20:17 -0400

    Add comment delete & edit options plus authorization logic to prevent others from messing with your comments.

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,7 @@
 var express = require("express");
-var router = express.Router({mergeParams: true});
+var router = express.Router({
+    mergeParams: true
+});
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 
@@ -44,12 +46,63 @@ router.post("/", isLoggedIn, function (req, res) {
     });
 });
 
+//Comment edit
+router.get("/:comment_id/edit", isCommentOwner, function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, comment) {
+        if (err) {
+            res.redirect("back")
+        } else {
+            res.render("comments/edit", {
+                campground_id: req.params.id,
+                comment: comment
+            });
+        }
+    });
+});
+
+//Comment update
+router.put("/:comment_id", isCommentOwner, function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, comment) {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect("/campgrounds/" + req.params.id)
+    });
+});
+
+//Comment destroy
+router.delete("/:comment_id", isCommentOwner, function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        res.redirect("/campgrounds/" + req.params.id);
+    });
+})
+
 //Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     } else {
         res.redirect("/login");
+    }
+}
+
+function isCommentOwner(req, res, next) {
+    //is logged in
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function (err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                //is owner
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
     }
 }
 
